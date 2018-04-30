5d6acbaf45bcf8d3a9091f0731fae2b651bb3643
RotCet <grousss@hotmail.com>
2018-04-30 12:53:21 -0500

    contactos

--- AplicacioonAngular/contactos/src/app/app.component.ts
+++ AplicacioonAngular/contactos/src/app/app.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore} from 'angularfire2/firestore';
+import { AngularFireDatabase,AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import {FirebaseListObservable} from 'angularfire2/database-deprecated';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <ul>
+    <li *ngFor="let item of items | async">
+      {{ item.Nombre }}
+    </li>
+  </ul>
+  `
+  //selector: 'app-root',
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Aprendiento Angular';
   items: Observable<any[]>;
-  constructor(db: AngularFirestore) 
-  {
-    this.items = db.collection('items').valueChanges();
+  constructor(db: AngularFirestore) {
+    this.items =  db.collection('contactos').valueChanges()
   }
+
+ // constructor(db: AngularFireDatabase) {
+ //   this.items = db..list('/contactos');
+ // }
 }
--- AplicacioonAngular/contactos/src/app/app.module.ts
+++ AplicacioonAngular/contactos/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 
 import { AppComponent } from './app.component';
-
 export const environment = {
   production: false,
   firebase: {
@@ -19,6 +22,7 @@ export const environment = {
   }
 };
 
+
 @NgModule({
   declarations: [
     AppComponent
@@ -27,7 +31,7 @@ export const environment = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase,'contactos')
   ],
   providers: [],
   bootstrap: [AppComponent]
